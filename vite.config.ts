29ecbb4df2f1269167d77d91d2ce3c525bd5a532
MD Nabeel Ahemad <nabeel.ahemad@yusata.com>
2026-04-28 16:09:38 +0530

    fix: remove platform-specific rollup issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['recharts'],
+  },
+  ssr: {
+    noExternal: ['recharts'],
+  },
 })
